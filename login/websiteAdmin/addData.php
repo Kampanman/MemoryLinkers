d5aa7226b15524345d30fdcdd9dcf769722023db
Kampanman <55080133+Kampanman@users.noreply.github.com>
2020-11-23 02:04:40 +0900

    Add files via upload

--- login/websiteAdmin/addData.php
+++ login/websiteAdmin/addData.php
@@ -28,6 +28,7 @@ $add_input1 = 'ウェブサイトURL';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
     <style>
     .info:hover, .info2:hover{
       cursor: pointer;
